cd6353594d20c58cef11dfe2f9be16e2c43d14c6
ThikerXCX <rifkimuhammad967@gmail.com>
2021-11-23 20:20:27 +0700

    tambah form login

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/login', function () {
+    return view('auth/login');
+});
 Route::get('/about', function () {
     return view(
         'about',
