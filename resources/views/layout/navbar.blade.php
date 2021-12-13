584fe93603b6b4440b23eee75b510f6abda2c988
ThikerXCX <rifkimuhammad967@gmail.com>
2021-12-13 19:35:22 +0700

    buat middleware + logout

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -4,28 +4,60 @@
             <ul class="navbar-nav">
                 @foreach($navbars as $navbar => $url)
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="{{ $url }}">{{ $navbar }}</a>
+                    <a class="nav-link" aria-current="page" href="{{ $url }}">{{
+                        $navbar
+                    }}</a>
                 </li>
                 @endforeach
             </ul>
             <ul class="navbar-nav ms-auto">
+                @guest
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="{{ route('register') }}">Register</a>
+                    <a
+                        class="nav-link"
+                        aria-current="page"
+                        href="{{ route('register') }}"
+                        >Register</a
+                    >
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="{{ route('login') }}">Login</a>
+                    <a
+                        class="nav-link"
+                        aria-current="page"
+                        href="{{ route('login') }}"
+                        >Login</a
+                    >
                 </li>
+                @else
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
+                    <a
+                        class="nav-link dropdown-toggle"
+                        href="#"
+                        id="navbarDropdown"
+                        role="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                    >
+                        {{ Auth::user()->name }}
                     </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <li><a class="dropdown-item" href="#">Action</a></li>
-                      <li><a class="dropdown-item" href="#">Another action</a></li>
-                      <li><hr class="dropdown-divider"></li>
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    <ul
+                        class="dropdown-menu dropdown-menu-end"
+                        aria-labelledby="navbarDropdown"
+                    >
+                        <li><a class="dropdown-item" href="#">Dasboard</a></li>
+                        <li>
+                            <a class="dropdown-item" href="#">Another action</a>
+                        </li>
+                        <li><hr class="dropdown-divider" /></li>
+                        <li>
+                            <form action="{{ route('logout') }}" method="post">
+                                @csrf
+                                <button type="submit" class="dropdown-item">Logout</button>
+                            </form>
+                        </li>
                     </ul>
-                  </li>
+                </li>
+                @endguest
             </ul>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BeritaController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\RegistrationController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,12 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-Route::get('login', [LoginController::class, 'create'])->name('login');
-Route::post('login', [LoginController::class, 'store']);
-
-Route::get('register', [RegistrationController::class, 'create'])->name('register');
-Route::post('register', [RegistrationController::class, 'store']); // bisa tetap pake route karena pad form menggunakan method post
-
 Route::get('/about', function () {
     return view(
         'about',
@@ -36,3 +31,13 @@ Route::get('/about', function () {
 });
 Route::get('/news', [BeritaController::class, 'index'])->name('news');
 Route::get('/news/newsPost:slug', [BeritaController::class, 'show']);
+
+Route::middleware('guest')->group(function(){
+    Route::get('login', [LoginController::class, 'create'])->name('login')->middleware('guest');
+    Route::post('login', [LoginController::class, 'store']);
+
+    Route::get('register', [RegistrationController::class, 'create'])->name('register');//bisa menggunakan->middleware('guest') setelah nama
+    Route::post('register', [RegistrationController::class, 'store']); // bisa tetap pake route karena pad form menggunakan method post
+});
+
+Route::post('logout',LogoutController::class)->middleware('auth')->name('logout');
