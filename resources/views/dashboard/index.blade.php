e8682843f39e87dfe20b22c8870cdca05fead1c0
ThikerXCX <rifkimuhammad967@gmail.com>
2021-12-13 20:57:02 +0700

    fix dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -43,10 +43,7 @@
                         ></a>
                     </li>
                     <li class="nav-item d-none d-sm-inline-block">
-                        <a href="../../index3.html" class="nav-link">Home</a>
-                    </li>
-                    <li class="nav-item d-none d-sm-inline-block">
-                        <a href="#" class="nav-link">Contact</a>
+                        <a href="/" class="nav-link">Home</a>
                     </li>
                 </ul>
 
@@ -66,31 +63,14 @@
             <!-- /.navbar -->
 
             <!-- Main Sidebar Container -->
-            <aside
-                class="
-                    main-sidebar main-sidebar-custom
-                    sidebar-dark-primary
-                    elevation-4
-                "
-            >
-                <!-- Brand Logo -->
-                <a href="../../index3.html" class="brand-link">
-                    <img
-                        src="../../dist/img/AdminLTELogo.png"
-                        alt="AdminLTE Logo"
-                        class="brand-image img-circle elevation-3"
-                        style="opacity: 0.8"
-                    />
-                    <span class="brand-text font-weight-light">PostS </span>
-                </a>
-
+            <aside class="main-sidebar main-sidebar-custom sidebar-dark-primary elevation-4">
                 <!-- Sidebar -->
                 <div class="sidebar">
                     <!-- Sidebar user (optional) -->
                     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                         <div class="image">
                             <img
-                                src="../../dist/img/user2-160x160.jpg"
+                                src="img/IMG_6912.jpg"
                                 class="img-circle elevation-2"
                                 alt="User Image"
                             />
