42ed4ca6a82fc92074c2d96180300ab1845ebb01
ThikerXCX <rifkimuhammad967@gmail.com>
2021-12-14 05:18:43 +0700

    perbaiki navbar

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -6,52 +6,29 @@
         <title>AdminLTE 3 | Pages</title>
 
         <!-- Google Font: Source Sans Pro -->
-        <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"
-        />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"/>
         <!-- Font Awesome -->
-        <link
-            rel="stylesheet"
-            href="../../plugins/fontawesome-free/css/all.min.css"
-        />
+        <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css"/>
         <!-- overlayScrollbars -->
-        <link
-            rel="stylesheet"
-            href="../../plugins/overlayScrollbars/css/OverlayScrollbars.min.css"
-        />
+        <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css" />
         <!-- Theme style -->
-        <link rel="stylesheet" href="../../dist/css/adminlte.min.css" />
+        <link rel="stylesheet" href="dist/css/adminlte.min.css" />
     </head>
     <body class="hold-transition sidebar-mini layout-fixed">
             <!-- Navbar -->
-            <nav
-                class="
-                    main-header
-                    navbar navbar-expand navbar-white navbar-light
-                "
-            >
+            <nav class="main-header navbar navbar-expand navbar-white navbar-light">
                 <!-- Left navbar links -->
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a
-                            class="nav-link"
-                            data-widget="pushmenu"
-                            href="#"
-                            role="button"
-                            ><i class="fas fa-bars"></i
-                        ></a>
+                        <a class="nav-link" data-widget="pushmenu" href="#" role="button" >
+                            <i class="fas fa-bars"></i>
+                        </a>
                     </li>
                     <li class="nav-item d-none d-sm-inline-block">
                         <a href="/" class="nav-link">Home</a>
                     </li>
                 </ul>
-
-                <!-- Right navbar links -->
                 <ul class="navbar-nav ml-auto">
-                    <!-- Navbar Search -->
-
-                    <!-- Notifications Dropdown Menu -->
                     <li class="nav-item">
                         <form action="{{ route('logout') }}" method="post">
                             @csrf
@@ -60,33 +37,33 @@
                     </li>
                 </ul>
             </nav>
-            <!-- /.navbar -->
-
-            <!-- Main Sidebar Container -->
             <aside class="main-sidebar main-sidebar-custom sidebar-dark-primary elevation-4">
                 <!-- Sidebar -->
                 <div class="sidebar">
                     <!-- Sidebar user (optional) -->
                     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                         <div class="image">
-                            <img
-                                src="img/IMG_6912.jpg"
-                                class="img-circle elevation-2"
-                                alt="User Image"
-                            />
+                            <img src="img/IMG_6913.jpg" class="img-circle elevation-2" alt="User Image"/>
                         </div>
                         <div class="info">
                             <a href="{{ route('dashboard') }}" class="d-block">{{ Auth::user()->name }}</a>
                         </div>
                     </div>
-
                     <!-- Sidebar Menu -->
                     <nav class="mt-2">
                         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                             <!-- Add icons to the links using the .nav-icon classwith font-awesome or any other icon font library -->
                             <li class="nav-item">
-                                <a href="../widgets.html" class="nav-link">
-                                    <i class="nav-icon fas fa-th"></i>
+                                <a href="{{ route('dashboard') }}" class="nav-link">
+                                    <i class="nav-icon fas fa-chalkboard-teacher"></i>
+                                    <p>
+                                        Dashboard
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ 'berita' }}" class="nav-link">
+                                    <i class="nav-icon fab fa-buffer"></i>
                                     <p>
                                         Berita
                                     </p>
@@ -96,8 +73,6 @@
                     </nav>
                     <!-- /.sidebar-menu -->
                 </div>
-                <!-- /.sidebar -->
-                <!-- /.sidebar-custom -->
             </aside>
 
             <!-- Content Wrapper. Contains page content -->
